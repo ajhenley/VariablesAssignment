826a28356c45a966613ab8c85dffc8c0c355e7d1
Alton J Henley Jr <ajhenley@gmail.com>
2020-08-31 16:20:11 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        int x; // 1, 2, 1000
+        float y = 3.14f; // 3.14,  6.23, 1.00
+        String n; // "123", "bob"
+        boolean b; // True, False
+        char c; // "s", "d"
+        byte by; //
+        short s; // -32768 - 32767   2bytes
+        long l; // 4bytes
+        double d; //
+
+        int i;
+        i = 3;
+        int j = 4;
+        System.out.println("Good Afternoon");
+    }
+}
